2d1a1de3145aa0d352150f3a2abfeec8cdb1d982
burryfun <kiril0108@gmail.com>
2022-09-03 20:43:23 +0600

    Add project files.

---
+++ DummyDataSenderToWebShop/Program.cs
@@ -0,0 +1,13 @@
+﻿const string URL = "http://localhost:5054";
+
+//For all brands:
+//     Add brand...
+//         Add BrandImage
+
+//For all smartphones:
+//     Add smartphone...
+//         Add SmartphoneImage
+
+
+
+}
